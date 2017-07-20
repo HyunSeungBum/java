c8a3a3f52eafb0a7e45f019f633623b1a26e69e5
Hyun Seumg Bum <orion203@gmail.com>
2017-07-21 00:36:44 +0900

    Modified parameters of CLI
    Modified parameters of CLI

--- JBoss/6.x/initScript/add-user.sh
+++ JBoss/6.x/initScript/add-user.sh
@@ -4,7 +4,6 @@ BASE_DIR=$(dirname $0)
 
 . $BASE_DIR/jboss-env.sh
 
-export JAVA_OPTS=" -Djboss.server.config.user.dir=${JBOSS_NODE_BASE_DIR}/${JBOSS_NODE_NAME}/configuration $JAVA_OPTS"
 export JAVA_OPTS=" -Djava.awt.headless=false $JAVA_OPTS"
 
-$JBOSS_HOME/bin/add-user.sh $@
+$JBOSS_HOME/bin/add-user.sh -sc ${JBOSS_NODE_BASE_DIR}/${JBOSS_NODE_NAME}/configuration
